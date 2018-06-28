e30f2ddaa1fe30dcda9583cc71be8f410ad60aa6
qmau94 <qmau94@gmail.com>
2018-06-28 10:42:51 +0900

    remove redis

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
-var cache = require('express-redis-cache')();
+// var cache = require('express-redis-cache')();
 
 keystone.set('404', function (req, res, next) {
 	res.status(404).render('errors/404');
@@ -37,13 +37,20 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
-	app.get('/', cache.route({ expire: 2592000 }), routes.views.index);
-	app.get('/tag/:tag?', cache.route({ expire: 2592000 }), routes.views.tag);
-	app.get('/blog/:category?', cache.route({ expire: 2592000 }), routes.views.blog);
-	app.get('/blog/post/:post', cache.route({ expire: 2592000 }), routes.views.post);
-	app.get('/about', cache.route({ expire: 2592000 }), routes.views.about);
-	app.get('/bucket-list', cache.route({ expire: 2592000 }), routes.views.list);
-	app.get('/tos', cache.route({ expire: 2592000 }), routes.views.tos);
+	// app.get('/', cache.route({ expire: 2592000 }), routes.views.index);
+	// app.get('/tag/:tag?', cache.route({ expire: 2592000 }), routes.views.tag);
+	// app.get('/blog/:category?', cache.route({ expire: 2592000 }), routes.views.blog);
+	// app.get('/blog/post/:post', cache.route({ expire: 2592000 }), routes.views.post);
+	// app.get('/about', cache.route({ expire: 2592000 }), routes.views.about);
+	// app.get('/bucket-list', cache.route({ expire: 2592000 }), routes.views.list);
+	// app.get('/tos', cache.route({ expire: 2592000 }), routes.views.tos);
+	app.get('/', routes.views.index);
+	app.get('/tag/:tag?', routes.views.tag);
+	app.get('/blog/:category?', routes.views.blog);
+	app.get('/blog/post/:post', routes.views.post);
+	app.get('/about', routes.views.about);
+	app.get('/bucket-list', routes.views.list);
+	app.get('/tos', routes.views.tos);
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
